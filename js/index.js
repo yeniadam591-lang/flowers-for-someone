945185874efce4ad554502b4f7cf0ee221bf8c3d
yeniadam591-lang <yeniadam591@gmail.com>
2025-08-01 17:53:03 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `I Have Something, Ra`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
