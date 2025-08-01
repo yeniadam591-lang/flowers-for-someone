903b8dc05b622dde5eb2075187b9a3931e28f0e1
yeniadam591-lang <yeniadam591@gmail.com>
2025-08-01 17:40:49 +0700

    Update main.js
    P

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Happy gf day,sayang! I lovee u').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
